12a05f4d83f5797ddb0fe90d0331a6c75086586f
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2022-02-16 21:51:34 +0200

    navigate

--- src/app/pages/pickcup-call/pickcup-call.page.ts
+++ src/app/pages/pickcup-call/pickcup-call.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pickcup-call',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PickcupCallPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  addPickcupCall() {
+    this.router.navigate(['home']);
   }
 
 }
