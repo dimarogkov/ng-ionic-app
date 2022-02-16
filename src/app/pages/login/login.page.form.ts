87647b791e59949a176e04ac7b59a77e87849978
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2022-02-16 22:50:15 +0200

    form login

---
+++ src/app/pages/login/login.page.form.ts
@@ -0,0 +1,14 @@
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+export class LoginPageForm {
+    
+    constructor(private formBuilder: FormBuilder){}
+
+    createForm(): FormGroup {
+        return this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+}
